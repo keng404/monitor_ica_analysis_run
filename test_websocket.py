1fb12a2a8035abfa2f6d8e60a774bf6d9656a1ba
Ken Eng <keng@illumina.com>
2022-10-05 17:24:49 -0400

    adding hard-coded timeout to stream websockets

--- test_websocket.py
+++ test_websocket.py
@@ -181,7 +181,7 @@ def download_file(api_key,project_id,data_id,output_path):
  
 async def stream_log(uri,extra_headers,num_checks = 2):
     checks = 0
-    async with websockets.connect(uri,extra_headers=extra_headers) as ws:
+    async with websockets.connect(uri,extra_headers=extra_headers,ping_interval=None) as ws:
         while True and checks <= num_checks:
             checks += 1
             try:
