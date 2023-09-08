0ad37b59415a5c12f0205cbee408a957eb570d01
Ken Eng <keneng87@gmail.com>
2023-09-08 13:07:04 -0400

    adding ica_pipelines.check_out_workflow_metrics.R and identifying todos for this mini-project

---
+++ ica_pipelines.check_out_workflow_metrics.R
@@ -0,0 +1,145 @@
+library(DBI)
+library(RSQLite)
+library(rlog)
+options(stringsAsFactors=FALSE)
+suppressPackageStartupMessages(library("argparse"))
+parser <- ArgumentParser()
+
+# specify our desired options 
+# by default ArgumentParser will add an help option 
+parser$add_argument("-d", "--db-file","--db_file",required=TRUE,default = NULL,
+                    help="SQLite DB file that contains kubernetes pod metrics")
+args <- parser$parse_args()
+db_file = args$db_file
+if(is.null(db_file)){
+  stop(paste("Please provide a DB file to this script"))
+}
+if(!file.exists(db_file)){
+  stop(paste("Please provide a DB file to this script.\nYou provided",db_file,"\n"))
+  
+}
+con <- dbConnect(SQLite(), "/Users/KishMish/Downloads/metrics.db")
+
+# Show List of Tables
+as.data.frame(dbListTables(con))
+#dbListTables(con)
+#1 container_metrics
+#2        containers
+#3       disk_usages
+#4      file_systems
+#5              pods
+### https://stackoverflow.com/questions/54531646/checking-kubernetes-pod-cpu-and-memory-utilization
+memory_conversion_list = list()
+memory_conversion_list[['Ki']] = 1e3
+memory_conversion_list[['Mi']] = 1e6
+memory_conversion_list[['Gi']] = 1e9
+memory_numerical_conversion <- function(memory_metrics){
+  memory_metrics_updated = memory_metrics
+  for(j in 1:length(memory_metrics_updated)){
+    for(i in 1:length(names(memory_conversion_list))){
+      label_to_prune = names(memory_conversion_list)[i]
+      modified_value = gsub(label_to_prune,"",memory_metrics[j])
+      if(modified_value != memory_metrics[j]){
+        # return memory metrics in Gbytes
+        memory_metrics_updated[j] = (as.numeric(modified_value) * memory_conversion_list[[label_to_prune]])/1e9
+        break
+      }
+    }
+  }
+  return(memory_metrics_updated)
+}
+###### https://stackoverflow.com/questions/54531646/checking-kubernetes-pod-cpu-and-memory-utilization
+cpu_conversion_list = list()
+cpu_conversion_list[['m']] = 1e3
+cpu_numerical_conversion <- function(cpu_metrics){
+  cpu_metrics_updated = cpu_metrics
+  for(j in 1:length(cpu_metrics_updated)){
+    for(i in 1:length(names(cpu_conversion_list))){
+      label_to_prune = names(cpu_conversion_list)[i]
+      modified_value = gsub(label_to_prune,"",cpu_metrics[j])
+      if(modified_value != cpu_metrics[j]){
+        cpu_metrics_updated[j] = as.numeric(modified_value)/cpu_conversion_list[[label_to_prune]]
+        break
+      }
+    }
+  }
+  return(cpu_metrics_updated)
+}
+
+
+### associate analysis pod to container
+pods <- dbReadTable(con, 'pods')
+pod_labels_of_interest = c('uwf','nf','cwl')
+pipeline_task_pod = pods[grepl('uwf',pods$name),]$id
+runner_pod = pods[grepl('nf|cwl',pods$name),]$id
+analysis_id = strsplit(gsub("bpe1-","",pods[grepl('monitor',pods$name),]$name),"\\-monitor")[[1]][1]
+#pods
+
+### get labels for each container
+containers <- dbReadTable(con, 'containers')
+container_labels = c('task','nf','cwl')
+pipeline_task_container = containers[grepl('task',containers$name) & containers$pod_id == pipeline_task_pod,]$id
+runner_container = containers[grepl('nf|cwl',containers$name)& containers$pod_id == runner_pod,]$id
+#containers
+
+# Get table
+### nemory and cpu usage
+container_metrics <- dbReadTable(con, 'container_metrics')
+#container_metrics
+
+library(ggplot2)
+library(lubridate)
+
+time_x = lubridate::ymd_hms(container_metrics[container_metrics$container_id == pipeline_task_container,]$timestamp)
+measurement_y = memory_numerical_conversion(container_metrics[container_metrics$container_id == pipeline_task_container,]$mem_usage) 
+p <- ggplot() + aes(x=time_x,y=as.numeric(measurement_y))  + geom_line(linetype = "dashed") + geom_point()
+
+rlog::log_info(paste("Creating PDF for task memory usage:",paste0("analysis_",analysis_id,".task.memory_consumption.pdf")))
+
+pdf(paste0("analysis_",analysis_id,".task.memory_consumption.pdf"))
+p + ggtitle(paste("Memory consumption of pipeline")) + xlab("timestamp") + ylab("Memory consumption in Gb")
+dev.off()
+
+time_x = lubridate::ymd_hms(container_metrics[container_metrics$container_id == runner_container,]$timestamp)
+measurement_y = memory_numerical_conversion(container_metrics[container_metrics$container_id == runner_container,]$mem_usage) 
+p <- ggplot() + aes(x=time_x,y=as.numeric(measurement_y))  + geom_line(linetype = "dashed") + geom_point()
+
+rlog::log_info(paste("Creating PDF for pipeline memory usage:",paste0("analysis_",analysis_id,".workflow_runner.memory_consumption.pdf")))
+
+pdf(paste0("analysis_",analysis_id,".workflow_runner.memory_consumption.pdf"))
+p + ggtitle(paste("Memory consumption of runner")) + xlab("timestamp") + ylab("Memory consumption in Gb")
+dev.off()
+
+#cpu_numerical_conversion(container_metrics[container_metrics$container_id == 4,]$cpu_usage)
+
+time_x = lubridate::ymd_hms(container_metrics[container_metrics$container_id == pipeline_task_container,]$timestamp)
+measurement_y = cpu_numerical_conversion(container_metrics[container_metrics$container_id == pipeline_task_container,]$cpu_usage) 
+p <- ggplot() + aes(x=time_x,y=as.numeric(measurement_y))  + geom_line(linetype = "dashed") + geom_point()
+
+rlog::log_info(paste("Creating PDF for task cpu usage:",paste0("analysis_",analysis_id,".task.cpu_consumption.pdf")))
+
+pdf(paste0("analysis_",analysis_id,".task.cpu_consumption.pdf"))
+p + ggtitle(paste("CPU consumption of pipeline")) + xlab("timestamp") + ylab("CPU usage")
+dev.off()
+
+time_x = lubridate::ymd_hms(container_metrics[container_metrics$container_id == runner_container,]$timestamp)
+measurement_y = cpu_numerical_conversion(container_metrics[container_metrics$container_id == runner_container,]$cpu_usage) 
+p <- ggplot() + aes(x=time_x,y=as.numeric(measurement_y))  + geom_line(linetype = "dashed") + geom_point()
+
+rlog::log_info(paste("Creating PDF for workflow cpu usage:",paste0("analysis_",analysis_id,".workflow_runner.cpu_consumption.pdf")))
+pdf(paste0("analysis_",analysis_id,".workflow_runner.cpu_consumption.pdf"))
+p + ggtitle(paste("CPU consumption of runner")) + xlab("timestamp") + ylab("CPU usage")
+dev.off()
+
+### initial disk size configuration
+file_systems <- dbReadTable(con, 'file_systems')
+#file_systems
+
+#### disk usage monitoring
+disk_usages <- dbReadTable(con, 'disk_usages')
+analysis_disk_usage = disk_usages[grepl("ces",disk_usages$mount_on),]
+p <- ggplot() + aes(x=lubridate::ymd_hms(analysis_disk_usage$created_at),y=as.numeric(analysis_disk_usage$used_pct))  + geom_line(linetype = "dashed") + geom_point()
+rlog::log_info(paste("Creating PDF for disk usage:",paste0("analysis_",analysis_id,".disk_usage.pdf")))
+pdf(paste0("analysis_",analysis_id,".disk_usage.pdf"))
+p + ggtitle(paste("analysis_disk_usage")) + xlab("timestamp") + ylab("disk usage %")
+dev.off()
